69da3f03439237ab171d11cf4f394486f76fb253
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 09:50:42 +0200

    Create Wheels.py

---
+++ Wheels.py
@@ -0,0 +1,27 @@
+from gpiozero import Servo
+from time import sleep
+
+# Create a Servo object and specify the GPIO pin where your servo is connected
+servo = Servo(8)  # Adjust the GPIO pin (17) to match your setup
+
+# Define the angle for forward and backward positions
+forward_angle = 90
+backward_angle = -90
+
+try:
+    while True:
+        # Move the servo forward for 2 seconds
+        servo.value = forward_angle / 180.0
+        sleep(2)
+
+        # Move the servo backward for 3 seconds
+        servo.value = backward_angle / 180.0
+        sleep(3)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Reset the servo to the neutral position (optional)
+    servo.value = None
+    servo.close()
