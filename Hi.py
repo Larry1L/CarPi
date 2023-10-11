18d97dc164b2f6e7afcf6561885aa07fd2144222
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 11:51:14 +0200

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 import time
-
+LED = 14
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(14,GPIO.OUT)
+GPIO.setup(LED,GPIO.OUT)
 
 # While loop
 while True:
         # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.HIGH)
+        GPIO.output(LED,GPIO.HIGH)
         # show message to Terminal
         print "LED is ON"
         # pause for one second
@@ -16,7 +16,7 @@ while True:
 
 
         # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.LOW)
+        GPIO.output(LED,GPIO.LOW)
         # show message to Terminal
         print "LED is OFF"
         # pause for one second
