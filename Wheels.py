d4a0751568fe8dcefc21546d9dd2d8d393de3a41
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 12:26:36 +0200

    Update Wheels.py

--- Wheels.py
+++ Wheels.py
@@ -1,39 +1,53 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO pin number (pin 8)
+# Set the GPIO pin numbers (pin 8 & 14)
 motorR_pin = 8
-
-# Configure the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Set up the motor pin as an output
-GPIO.setup(motor_pin, GPIO.OUT)
-
-try:
-    while True:  # Loop indefinitely
-        GPIO.output(motor_pin, GPIO.HIGH)  # Start the motor
-        time.sleep(1)  # Run the motor for 1 second
-        print("test")
-        GPIO.output(motor_pin, GPIO.LOW)  # Stop the motor
-        time.sleep(1)  # Wait for 1 second
-
-# Set the GPIO pin number (pin 8)
 motorL_pin = 14
 
 # Configure the GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-# Set up the motor pin as an output
-GPIO.setup(motor_pin, GPIO.OUT)
+# Set up the motor pins as an output
+GPIO.setup(motorR_pin, GPIO.OUT)
+GPIO.setup(motorL_pin, GPIO.OUT)
 
-try:
+
+def TurnRight():
+    try:
     while True:  # Loop indefinitely
-        GPIO.output(motor_pin, GPIO.HIGH)  # Start the motor
-        time.sleep(1)  # Run the motor for 1 second
-        print("test")
-        GPIO.output(motor_pin, GPIO.LOW)  # Stop the motor
-        time.sleep(1)  # Wait for 1 second
+        GPIO.output(motorR_pin, GPIO.HIGH)  # Start the motor
+        time.sleep(0.1)  # Run the motor for 1 second
+        print("Turning Right")
+
+def TurnLeft():
+    try:
+    while True:  # Loop indefinitely
+        GPIO.output(motorL_pin, GPIO.HIGH)  # Start the motor
+        time.sleep(0.1)  # Run the motor for 1 second
+        print("Turning Left")
+def BrakeRight():
+    try:
+    while True:  # Loop indefinitely
+        GPIO.output(motorR_pin, GPIO.LOW)  # Start the motor
+        time.sleep(0.1)  # Run the motor for 1 second
+        print("Turning Right")
+
+def BrakeLeft():
+    try:
+    while True:  # Loop indefinitely
+        GPIO.output(motorL_pin, GPIO.LOW)  # Start the motor
+        time.sleep(0.1)  # Run the motor for 1 second
+        print("Turning Left")
+def DriveForward():
+   try:
+       while True: # Loop indefinitely
+           GPIO.output(motorL_pin, GPIO.HIGH) # Starting both motors 
+           GPIO.output(motorR_pin, GPIO.HIGH)
+           print("Driving Forward")
+try:
+    while True:
+    DriveForward():
 
 except KeyboardInterrupt:
     pass
