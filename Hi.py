dfc5ff02c6c9306665d06aeb2ed470ce0a74951d
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 12:38:35 +0200

    Update Hi.py

--- Hi.py
+++ Hi.py
@@ -1,23 +1,58 @@
 import RPi.GPIO as GPIO
 import time
-LED = 8
+
+# Set the GPIO pin numbers (pin 8 & 14)
+motorR_pin = 8
+motorL_pin = 14
+
+# Configure the GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(LED,GPIO.OUT)
 
-# While loop
-while True:
-        # set GPIO14 pin to HIGH
-        GPIO.output(LED,GPIO.HIGH)
-        # show message to Terminal
-        print("LED is ON")
-        # pause for one second
-        time.sleep(1)
+# Set up the motor pins as an output
+GPIO.setup(motorR_pin, GPIO.OUT)
+GPIO.setup(motorL_pin, GPIO.OUT)
 
+# Create PWM instances for the left and right motors
+motorR_pwm = GPIO.PWM(motorR_pin, 100)  # PWM frequency (Hz)
+motorL_pwm = GPIO.PWM(motorL_pin, 100)
 
-        # set GPIO14 pin to HIGH
-        GPIO.output(LED,GPIO.LOW)
-        # show message to Terminal
-        print("LED is OFF")
-        # pause for one second
-        time.sleep(1)
+# Start PWM with 0% duty cycle (stopped)
+motorR_pwm.start(0)
+motorL_pwm.start(0)
+
+def TurnRight():
+    motorR_pwm.ChangeDutyCycle(50)  # Adjust duty cycle to control speed
+    motorL_pwm.ChangeDutyCycle(100)  # Full speed
+    time.sleep(1)
+    motorR_pwm.ChangeDutyCycle(0)  # Stop
+    motorL_pwm.ChangeDutyCycle(0)
+    time.sleep(1)
+
+def TurnLeft():
+    motorR_pwm.ChangeDutyCycle(100)  # Full speed
+    motorL_pwm.ChangeDutyCycle(50)  # Adjust duty cycle to control speed
+    time.sleep(1)
+    motorR_pwm.ChangeDutyCycle(0)  # Stop
+    motorL_pwm.ChangeDutyCycle(0)
+    time.sleep(1)
+
+def DriveForward():
+    motorR_pwm.ChangeDutyCycle(75)  # 75% speed for the right motor
+    motorL_pwm.ChangeDutyCycle(100)  # Full speed for the left motor
+    time.sleep(1)
+    motorR_pwm.ChangeDutyCycle(0)  # Stop
+    motorL_pwm.ChangeDutyCycle(0)
+    time.sleep(1)
+
+try:
+    while True:
+        DriveForward()
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Clean up GPIO settings
+    motorR_pwm.stop()
+    motorL_pwm.stop()
+    GPIO.cleanup()
