2616e873b7280f8fe03cf972134ec37bdfed7f18
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 09:43:28 +0200

    Update LineSensorTest.py

--- LineSensorTest.py
+++ LineSensorTest.py
@@ -3,19 +3,19 @@ import time
 
 # Configure GPIO pins
 GPIO.setmode(GPIO.BCM)
-sensor_pin = 17  # Example GPIO pin, adjust to your setup
+sensor_pin = 2  # Example GPIO pin, adjust to your setup
 
 # Set up the sensor pin as an input
 GPIO.setup(sensor_pin, GPIO.IN)
 
 try:
-    if GPIO.input(sensor_pin) == GPIO.HIGH:
+    if GPIO.input(sensor_pin) == GPIO.LOW:
         print("Sensor is detecting a line at startup")
     else:
         print("Sensor is not detecting a line at startup")
 
     while True:
-        if GPIO.input(sensor_pin) == GPIO.HIGH:
+        if GPIO.input(sensor_pin) == GPIO.LOW:
             print("Line detected")
         else:
             print("No line detected")
