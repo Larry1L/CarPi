f2b8c355c2e362ddad95f96a143a8d204101ddbc
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 11:31:12 +0200

    Update LEDTest.py

--- LEDTest.py
+++ LEDTest.py
@@ -1,30 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-
-# Set the GPIO pin number (pin 8)
-power_pin = 8
-
-# Configure the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Set up the power pin as an output
-GPIO.setup(power_pin, GPIO.OUT)
-
-try:
-    while True:  # Loop indefinitely
-        GPIO.output(power_pin, GPIO.HIGH)  # Turn on power
-        time.sleep(5)  # Power on for 5 second
-        GPIO.output(power_pin, GPIO.LOW)  # Turn off power
-        time.sleep(5)  # Wait for 5 seconds
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    # Clean up GPIO settings
-    GPIO.cleanup()
-import RPi.GPIO as GPIO
-import time
 # Set the GPIO pin number (pin 8)
 power_pin = 8
 
