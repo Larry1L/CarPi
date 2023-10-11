db6af67d477a1b6c64704b6cab8c3f70ceb393a0
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-11 12:17:41 +0200

    Update Wheels.py

--- Wheels.py
+++ Wheels.py
@@ -2,7 +2,24 @@ import RPi.GPIO as GPIO
 import time
 
 # Set the GPIO pin number (pin 8)
-motor_pin = 14
+motorR_pin = 8
+
+# Configure the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up the motor pin as an output
+GPIO.setup(motor_pin, GPIO.OUT)
+
+try:
+    while True:  # Loop indefinitely
+        GPIO.output(motor_pin, GPIO.HIGH)  # Start the motor
+        time.sleep(1)  # Run the motor for 1 second
+        print("test")
+        GPIO.output(motor_pin, GPIO.LOW)  # Stop the motor
+        time.sleep(1)  # Wait for 1 second
+
+# Set the GPIO pin number (pin 8)
+motorL_pin = 14
 
 # Configure the GPIO mode
 GPIO.setmode(GPIO.BCM)
