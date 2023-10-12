1e12f2c0181ef11dcd0a235312b33378adcfd6c2
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-12 14:29:29 +0200

    Update Wheels.py

--- Wheels.py
+++ Wheels.py
@@ -1,58 +1,9 @@
-import RPi.GPIO as GPIO
 import keyboard
-import time
 
-# Define GPIO pin numbers for your motors
-motorL_pin1 = 17  # Replace with your actual GPIO pin
-motorL_pin2 = 18  # Replace with your actual GPIO pin
-motorR_pin1 = 19  # Replace with your actual GPIO pin
-motorR_pin2 = 20  # Replace with your actual GPIO pin
+def on_key_event(e):
+    if e.name in ['w', 'a', 's', 'd']:
+        print("test")
 
-# Configure GPIO pins for motor control
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(motorL_pin1, GPIO.OUT)
-GPIO.setup(motorL_pin2, GPIO.OUT)
-GPIO.setup(motorR_pin1, GPIO.OUT)
-GPIO.setup(motorR_pin2, GPIO.OUT)
+keyboard.on_press(on_key_event)
 
-# Define motor control functions
-def FullSpeed():
-    GPIO.output(motorL_pin1, GPIO.HIGH)
-    GPIO.output(motorL_pin2, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.HIGH)
-    GPIO.output(motorR_pin2, GPIO.LOW)
-
-def TurnLeft():
-    GPIO.output(motorL_pin1, GPIO.LOW)
-    GPIO.output(motorL_pin2, GPIO.HIGH)
-    GPIO.output(motorR_pin1, GPIO.HIGH)
-    GPIO.output(motorR_pin2, GPIO.LOW)
-
-def TurnRight():
-    GPIO.output(motorL_pin1, GPIO.HIGH)
-    GPIO.output(motorL_pin2, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.LOW)
-    GPIO.output(motorR_pin2, GPIO.HIGH)
-
-def StopDriving():
-    GPIO.output(motorL_pin1, GPIO.LOW)
-    GPIO.output(motorL_pin2, GPIO.LOW)
-    GPIO.output(motorR_pin1, GPIO.LOW)
-    GPIO.output(motorR_pin2, GPIO.LOW)
-
-try:
-    while True:
-        if keyboard.is_pressed('w'):
-            FullSpeed()
-        elif keyboard.is_pressed('a'):
-            TurnLeft()
-        elif keyboard.is_pressed('d'):
-            TurnRight()
-        else:
-            StopDriving()
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    pass
-finally:
-    StopDriving()
-    GPIO.cleanup()
+keyboard.wait()
