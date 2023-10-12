23fc33da59ee7ce151820bff507efe2f3cbd8276
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-12 14:35:38 +0200

    Update LineSensorTest.py

--- LineSensorTest.py
+++ LineSensorTest.py
@@ -1,33 +1,69 @@
 import RPi.GPIO as GPIO
+import keyboard
 import time
 
-# Configure GPIO pins
-GPIO.setmode(GPIO.BCM)
-sensorL_pin = 4  # Example GPIO pin, adjust to your setup
-sensorR_pin = 17 # Example GPIO pin, adjust to your setup
+# Define GPIO pin numbers for your motors
+motorL_pin1 = 17  # Replace with your actual GPIO pin
+motorL_pin2 = 18  # Replace with your actual GPIO pin
+motorR_pin1 = 19  # Replace with your actual GPIO pin
+motorR_pin2 = 20  # Replace with your actual GPIO pin
 
-# Set up the sensor pin as an input
-GPIO.setup(sensorL_pin, GPIO.IN)
-GPIO.setup(sensorR_pin, GPIO.IN)
+# Get the speed from the user at the start
+speed = float(input("Enter the speed (0.0 to 1.0): "))
+
+# Verify that the speed is within a valid range (0.0 to 1.0)
+if not (0.0 <= speed <= 1.0):
+    print("Invalid speed value. Please enter a value between 0.0 and 1.0.")
+    exit(1)
+
+# Configure GPIO pins for motor control
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motorL_pin1, GPIO.OUT)
+GPIO.setup(motorL_pin2, GPIO.OUT)
+GPIO.setup(motorR_pin1, GPIO.OUT)
+GPIO.setup(motorR_pin2, GPIO.OUT)
+
+# Calculate the speed values for FullSpeed based on user input
+motor_speed = int(speed * 100)  # Convert the speed to a percentage
+
+# Define motor control functions
+def FullSpeed():
+    GPIO.output(motorL_pin1, GPIO.HIGH)
+    GPIO.output(motorL_pin2, GPIO.LOW)
+    GPIO.output(motorR_pin1, GPIO.HIGH)
+    GPIO.output(motorR_pin2, GPIO.LOW)
+
+def TurnLeft():
+    GPIO.output(motorL_pin1, GPIO.LOW)
+    GPIO.output(motorL_pin2, GPIO.HIGH)
+    GPIO.output(motorR_pin1, GPIO.HIGH)
+    GPIO.output(motorR_pin2, GPIO.LOW)
+
+def TurnRight():
+    GPIO.output(motorL_pin1, GPIO.HIGH)
+    GPIO.output(motorL_pin2, GPIO.LOW)
+    GPIO.output(motorR_pin1, GPIO.LOW)
+    GPIO.output(motorR_pin2, GPIO.HIGH)
+
+def StopDriving():
+    GPIO.output(motorL_pin1, GPIO.LOW)
+    GPIO.output(motorL_pin2, GPIO.LOW)
+    GPIO.output(motorR_pin1, GPIO.LOW)
+    GPIO.output(motorR_pin2, GPIO.LOW)
 
 try:
-    if GPIO.input(sensorL_pin) == GPIO.LOW:
-        print("Left Sensor is detecting a line at startup")
-
-    if GPIO.input(sensorR_pin) == GPIO.LOW:
-        print("Right Sensor is detecting a line at startup")
-
     while True:
-        if GPIO.input(sensorL_pin) == GPIO.LOW:
-            print("Left Sensor - Line detected")
-
-        if GPIO.input(sensorR_pin) == GPIO.LOW:
-            print("Right Sensor - Line detected")
-
-        time.sleep(1)  # Adjust the sleep time as needed
-
+        if keyboard.is_pressed('w'):
+            FullSpeed()
+        elif keyboard.is_pressed('a'):
+            TurnLeft()
+        elif keyboard.is_pressed('d'):
+            TurnRight()
+        else:
+            StopDriving()
+        time.sleep(0.1)
 except KeyboardInterrupt:
     pass
-
 finally:
+    StopDriving()
     GPIO.cleanup()
