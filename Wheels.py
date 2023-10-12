91fb6b45372465955a01a058894b66f126673798
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-12 14:40:29 +0200

    Update Wheels.py

--- Wheels.py
+++ Wheels.py
@@ -2,29 +2,89 @@
 #python3 -m pip install sshkeyboard 
 
 from sshkeyboard import listen_keyboard
+import RPi.GPIO as GPIO # Raspberry GPIO library
+import time # Time library allowing for delays in the code
 
-def TurnLeft():
-    print("Turning Left")
+# Set the GPIO pin numbers (pin 8 & 14)
+motorR_pin = 8 # Left motors pin
+motorL_pin = 14 # Right motors pin
+DirR_pin = 27 # Right motors direction pin
+DirL_pin = 22 # Left motors direction pin
+sensorL_pin = 4  # GPIO pin for the left sensor
+sensorR_pin = 17  # GPIO pin for the right sensor
 
-def TurnRight():
+# Configure the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up the motor pins as an output
+GPIO.setup(motorR_pin, GPIO.OUT)
+GPIO.setup(motorL_pin, GPIO.OUT)
+GPIO.setup(DirR_pin, GPIO.OUT)
+GPIO.setup(DirL_pin, GPIO.OUT)
+# Set up the sensor pins as inputs
+GPIO.setup(sensorL_pin, GPIO.IN) 
+GPIO.setup(sensorR_pin, GPIO.IN)
+
+# Create PWM instances for the left and right motors
+motorR_pwm = GPIO.PWM(motorR_pin, 100)  # PWM Speed (%)
+motorL_pwm = GPIO.PWM(motorL_pin, 100)
+
+# Start PWM with 0% duty cycle (stopped)
+motorR_pwm.start(0)
+motorL_pwm.start(0)
+
+def TurnRight(speed=100): # Function for the car to turn right by making the left wheel drive backwards faster than the right wheel drives forward
+    GPIO.output(DirL_pin, GPIO.HIGH)
+    GPIO.output(DirR_pin, GPIO.LOW)
+    motorR_pwm.ChangeDutyCycle(0)  # Adjust duty cycle to control speed
+    motorL_pwm.ChangeDutyCycle(speed)  # Full speed
     print("Turning Right")
 
-def GoForward():
-    print("Going Forward")
+def TurnLeft(speed=90): # Function for the car to turn left by making the right wheel drive backwards faster than the left wheel drives forward
+    GPIO.output(DirR_pin, GPIO.HIGH)
+    GPIO.output(DirL_pin, GPIO.LOW)
+    motorR_pwm.ChangeDutyCycle(speed)  # Full speed
+    motorL_pwm.ChangeDutyCycle(0)  # Adjust duty cycle to control speed
+    print("Turning Left")
 
-def GoBackward():
-    print("reversing")
+def FullSpeed(speed=65): # Function for driving forward and making sure none of the wheels are backwards
+    GPIO.output(DirL_pin, GPIO.LOW)
+    GPIO.output(DirR_pin, GPIO.LOW)
+    motorR_pwm.ChangeDutyCycle(speed)  # Full speed for the right motor
+    motorL_pwm.ChangeDutyCycle(speed)  # Full speed for the left motor
+def FullSpeed2(): # Starting speed to go as fast as possible until the car hits something
+    FullSpeed(100)
+    print("Starting Motors")
+def StopDriving(): # Function to stop driving
+    motorR_pwm.ChangeDutyCycle(0)
+    motorL_pwm.ChangeDutyCycle(0)
+    print("Stopping Motors")
 
-def press(key):
-    if key == "w":
-        GoForward()
-    if key == "s":
-        GoBackward()
-    if key == "a":
-        TurnLeft()
-    if key == "d":
-        TurnRight()
+# Wait for the user to press Enter to start
+input("Press Enter to start...")
 
+try:
+    FullSpeed()
+    
+    while True:
+        listen_keyboard(on_press = press)
+        if GPIO.input(sensorL_pin) == GPIO.LOW and GPIO.input(sensorR_pin) == GPIO.LOW:
+            print("Both sensors - Line detected") # The left sensor reacts to the white line on the ground, turning right to not exit the ring
+            FullSpeed()
+        elif GPIO.input(sensorL_pin) == GPIO.LOW:
+            print("Left Sensor - Line detected") # The left sensor reacts to the white line on the ground, turning right to not exit the ring
+            TurnRight()
+        elif GPIO.input(sensorR_pin) == GPIO.LOW:
+            print("Right Sensor - Line detected") # The right sensor reacts to the white line on the ground, turning right to not exit the ring
+            TurnLeft()
+        else:
+            FullSpeed() # None of the sensors detect anything, allowing the car to drive forward
 
-while True:
-    listen_keyboard(on_press = press)
+except KeyboardInterrupt: # Emergency terminal stop button ( CTRL + C )
+    pass
+
+finally:
+    # Clean up GPIO settings
+    motorR_pwm.stop()
+    motorL_pwm.stop()
+    GPIO.cleanup()
