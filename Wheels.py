0952b6b3eaf71a6722e8786c838301bac3d3e0b3
Larry1L <147480126+Larry1L@users.noreply.github.com>
2023-10-12 15:12:56 +0200

    Update Wheels.py

--- Wheels.py
+++ Wheels.py
@@ -18,40 +18,40 @@ GPIO.setup(DirR_pin, GPIO.OUT)
 GPIO.setup(DirL_pin, GPIO.OUT)
 
 # Create PWM instances for the left and right motors
-motorR_pwm = GPIO.PWM(motorR_pin, 100)
-motorL_pwm = GPIO.PWM(motorL_pin, 100)
+motorR_pwm = GPIO.PWM(motorR_pin, 1000)
+motorL_pwm = GPIO.PWM(motorL_pin, 1000)
 
 # Define motor control functions
 def TurnRight(speed=100):
     GPIO.output(DirL_pin, GPIO.HIGH)
     GPIO.output(DirR_pin, GPIO.LOW)
     motorR_pwm.ChangeDutyCycle(0)
-    motorL_pwm.ChangeDutyCycle(100)
+    motorL_pwm.ChangeDutyCycle(speed)
     print("Turning Right")
 
 def TurnLeft(speed=90):
     GPIO.output(DirR_pin, GPIO.HIGH)
     GPIO.output(DirL_pin, GPIO.LOW)
-    motorR_pwm.ChangeDutyCycle(100)
+    motorR_pwm.ChangeDutyCycle(speed)
     motorL_pwm.ChangeDutyCycle(0)
     print("Turning Left")
 
 def FullSpeed(speed=100):
     GPIO.output(DirL_pin, GPIO.LOW)
     GPIO.output(DirR_pin, GPIO.LOW)
-    motorR_pwm.ChangeDutyCycle(100)
-    motorL_pwm.ChangeDutyCycle(100)
+    motorR_pwm.ChangeDutyCycle(speed)
+    motorL_pwm.ChangeDutyCycle(speed)
     print("Going Full Speed")
 
 def GoBackward(speed=100):
     GPIO.output(DirL_pin, GPIO.HIGH)
     GPIO.output(DirR_pin, GPIO.HIGH)
-    motorR_pwm.ChangeDutyCycle(100)
-    motorL_pwm.ChangeDutyCycle(100)
+    motorR_pwm.ChangeDutyCycle(speed)
+    motorL_pwm.ChangeDutyCycle(speed)
     print("Going Backward")
 
 # Define a simple function to handle key presses
-def on_key_press(key):
+def press(key):
     if key == 'w':
         FullSpeed()
     elif key == 's':
@@ -60,22 +60,8 @@ def on_key_press(key):
         TurnLeft()
     elif key == 'd':
         TurnRight()
-    else:
-        motorR_pwm.ChangeDutyCycle(50)
-        motorL_pwm.ChangeDutyCycle(50)
 
 while True:
-    listen_keyboard(on_press=on_key_press)
-    if key == 'w':
-        FullSpeed()
-    elif key == 's':
-        GoBackward()
-    elif key == 'a':
-        TurnLeft()
-    elif key == 'd':
-        TurnRight()
-    else:
-        motorR_pwm.ChangeDutyCycle(50)
-        motorL_pwm.ChangeDutyCycle(50)
+    listen_keyboard(on_press = press)
 
 # You can add cleanup and exit code here if needed
